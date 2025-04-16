4b1d37cc753382a404bbfa66f7016e0b76043198
ganesh <ganeshturate1.com>
2025-04-16 12:24:04 +0530

    updated models

--- src/models/match.model.js
+++ src/models/match.model.js
@@ -2,11 +2,12 @@ import { model, Schema } from 'mongoose'
 
 const matchSchema = new Schema({
     tournament:{type:Schema.Types.ObjectId,ref:'Tournament'},
-    team1: { type: { id: { type: Schema.Types.ObjectId, ref: 'Teams' }, batsmans: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, balls: { type: Number } } }], bowlers: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, wickets: { type: Number }, balls: { type: Number } } }] } },
+    team1: { type: { id: { type: Schema.Types.ObjectId, ref: 'Teams' }, batsmans: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, balls: { type: Number } } }], bowlers: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, wickets: { type: Number }, balls: { type: Number } } }] },balls:[{type:{run:{type:Number},ballType:{type:String}}}] },
     team2: { type: { type: Schema.Types.ObjectId, ref: 'Teams' } },
+    overs:{type:Number},
     venue: { type: String, required: true },
     pom: { type: Schema.Types.ObjectId, ref: 'Users' },
-    result: { type: { won: { type: Schema.Types.ObjectId, ref: 'Teams' }, loss: { type: Schema.Types.ObjectId, ref: 'Teams' }, result: { type: String }, batsmans: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, balls: { type: Number } } }], bowlers: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, wickets: { type: Number }, balls: { type: Number } } }] } }
+    result: { type: { won: { type: Schema.Types.ObjectId, ref: 'Teams' }, loss: { type: Schema.Types.ObjectId, ref: 'Teams' }, result: { type: String }, batsmans: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, balls: { type: Number } } }], bowlers: [{ type: { id: { type: Schema.Types.ObjectId, ref: 'Users' }, runs: { type: Number }, wickets: { type: Number }, balls: { type: Number } } }] },balls:[{type:{run:{type:Number},ballType:{type:String}}}]  }
 })
 
 const matchModel = new model('Match', matchSchema)
